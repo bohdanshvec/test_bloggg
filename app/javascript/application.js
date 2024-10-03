c72fe98465e7bf95bc6164262d87e9e595fbad4a
bohdanshvetc <dan6147@ukr.net>
2024-10-03 16:08:43 +0300

    1 commit

---
+++ app/javascript/application.js
@@ -0,0 +1,15 @@
+// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+
+// import Rails from "@rails/ujs"
+// import Turbolinks from "turbolinks"
+// import * as ActiveStorage from "@rails/activestorage"
+// import "channels"
+
+// Rails.start()
+// Turbolinks.start()
+// ActiveStorage.start()
+
+import '@hotwired/turbo-rails'
+import 'controllers'
+
+
---
+++ config/routes.rb
@@ -0,0 +1,22 @@
+Rails.application.routes.draw do
+
+  resources :questions#, onli: %i[index new edit create update destroy show]
+
+  # resources :questions, onli: [:ndex, :new, :edit, :create]
+
+  # get '/questions', to: 'questions#index'
+
+  # get '/questions/new', to: 'questions#new'
+
+  # get '/questions/:id/edit', to: 'questions#edit'
+
+  # post '/questions', to: 'questions#create'
+  
+
+  
+  root 'pages#index'
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+end
